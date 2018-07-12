27dcc480bd76c382c5a9fa93cef0512284d15956
Muhammad Rahmatullah <muhammadrahmatullah1@gmail.com>
2018-07-13 02:57:06 +0800

    add butterknife dependencies

--- app/src/main/java/com/rahmat/app/kamus/MainActivity.java
+++ app/src/main/java/com/rahmat/app/kamus/MainActivity.java
@@ -3,11 +3,28 @@ package com.rahmat.app.kamus;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.mancj.materialsearchbar.MaterialSearchBar;
+
+public class MainActivity extends AppCompatActivity implements MaterialSearchBar.OnSearchActionListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onSearchStateChanged(boolean enabled) {
+
+    }
+
+    @Override
+    public void onSearchConfirmed(CharSequence text) {
+
+    }
+
+    @Override
+    public void onButtonClicked(int buttonCode) {
+
     }
 }
