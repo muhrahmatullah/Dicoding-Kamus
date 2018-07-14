b141e96197e55929d1de5d137b36d0eeefe952f9
Muhammad Rahmatullah <muhammadrahmatullah1@gmail.com>
2018-07-14 15:56:19 +0800

    add icon assets

--- app/src/main/java/com/rahmat/app/kamus/MainActivity.java
+++ app/src/main/java/com/rahmat/app/kamus/MainActivity.java
@@ -2,15 +2,42 @@ package com.rahmat.app.kamus;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.mancj.materialsearchbar.MaterialSearchBar;
 
-public class MainActivity extends AppCompatActivity implements MaterialSearchBar.OnSearchActionListener{
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements
+        MaterialSearchBar.OnSearchActionListener{
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Kamus");
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
