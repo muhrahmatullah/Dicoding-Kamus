3003cf6e33ad08b5529eb975883af194258308f2
Muhammad Rahmatullah <muhammadrahmatullah1@gmail.com>
2018-07-25 10:50:10 +0700

    add detailscreen

---
+++ app/src/main/java/com/rahmat/app/kamus/KamusDetailActivity.java
@@ -0,0 +1,13 @@
+package com.rahmat.app.kamus;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class KamusDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kamus_detail);
+    }
+}
