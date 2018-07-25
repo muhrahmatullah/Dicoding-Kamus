1df0b3289521a2eabd1159cb735761caf463fbbd
Muhammad Rahmatullah <muhammadrahmatullah1@gmail.com>
2018-07-25 22:43:43 +0700

    add layout on splash screen

--- app/src/main/java/com/rahmat/app/kamus/SplashActivity.java
+++ app/src/main/java/com/rahmat/app/kamus/SplashActivity.java
@@ -3,10 +3,20 @@ package com.rahmat.app.kamus;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ProgressBar;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class SplashActivity extends AppCompatActivity {
+
+    @BindView(R.id.progress_bar)
+    ProgressBar progressBar;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        ButterKnife.bind(this);
     }
 }
